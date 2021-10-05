4ef49d311375e7b6f6735fcba1932d085d27971c
Margareta <margaretavida120@gmail.com>
2021-10-05 18:17:57 +0200

    first commit

---
+++ 0-4feladat.py
@@ -0,0 +1,60 @@
+#1.feladat
+"""
+num1 = int(input("Kérek egy számot!"))
+num2 = int(input("Kérek egy számot!"))
+
+b = num1<4 and num2!=6
+a=num1==5 or num2!=4
+c=num1<=5 or num2>=13
+print(b,a,c)
+
+#2.feladat
+
+F=False
+T=True
+
+print(f"\tA\tI\tB\tI\tA and B")
+print("---------------------------------------------")
+print(f"\t{F}\tI\t{F}\tI\tFalse")
+
+
+num = int(input("Kérek egy számot!"))
+if num>0 :
+    if num /2:
+        print("A szám pozitív és páros")
+    else: 
+        print("A szám páros és páratlan")
+elif num/2:
+    print("A szám negatív és páros!")
+else:
+    print("A szám negatí és páratlan!")
+
+
+
+num = int(input("Kérek egy számot!"))
+if num>0 :
+    print("A szám pozitív")
+else:
+    print("A szám negatív!")
+
+
+#4.feladat
+
+num1 = int(input("Kérem a tört számlálóját!"))
+num2 = int(input("Kérem a tört nevezőjét!"))
+
+if num2==0:
+    print("Nem lehet nulla a tört nevezője")
+else:
+    print(num1/num2)
+"""
+
+#feladat 5
+
+num = int(input("Kérek egy háromjegyű számot!"))
+num1
+
+
+if num<100 or num>=1000:
+    print("Ez a szám nem háromjegyű")
+elif num
