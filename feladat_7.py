ecc956e32abb84cddc73f24e6295516edfc64902
Margareta <margaretavida120@gmail.com>
2021-10-05 19:08:01 +0200

    vége

--- feladat_7.py
+++ feladat_7.py
@@ -3,13 +3,18 @@ num2=int(input("Kérek egy számot"))
 
 if num1==num2:
     print("A két szám egyenlő")
+
 if (num1%2)!=0 and (num2%2)!=0:
     print("Mind a két szám páratlan.")
+
 if (num1%3)==0 or(num2%3)==0:
     print("Legalább az egyik szám osztható hárommal")
+
 if num1<0 and num2<0:
     print("MInd a két szám negatív")
+
 if num1<0 and num2>=0:
     print("Az egyik szám negatív, a másik szám pozitív.")
 elif num1>=0 and num2>0:
     print("Az egyik szám negatív, a másik szám pozitív.")
+
